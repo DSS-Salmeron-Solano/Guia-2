c703761049cf632a83ec9388d4f399f4f6cc9658
Salmerón Alvarado <77812307+DanielSalmeron3b@users.noreply.github.com>
2022-02-10 16:20:18 -0600

    Cambios en discusion_2.php

--- discusion_2.php
+++ discusion_2.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dólares a euros</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 
 $vocales = [
@@ -19,24 +32,34 @@ $simbolos = [
     '%', '&',
 ];
 
-$dato_usuario = "#";
-
 for ($i=0; $i < count($vocales); $i++) { 
     if ($dato_usuario == $vocales[$i]) {
-        echo "El dato ingresado es una vocal";
+        $resultado = "El dato ingresado es una vocal";
         break;
     }
 }
 for ($j=0; $j < count($numeros); $j++) { 
     if ($dato_usuario === $numeros[$j]) {
-        echo "El dato ingresado es un número";
+        $resultado ="El dato ingresado es un número";
         break;
     }
 }
 for ($k=0; $k < count($simbolos); $k++) { 
     if ($dato_usuario == $simbolos[$k]){
-        echo "El dato ingresado es un símbolo";
+        $resultado = "El dato ingresado es un símbolo";
         break;
     }
 }
 
+
+if(isset($_POST['submit']) && $_POST['submit'] == "Enviar"){
+    echo "\t<tr class=\"odd\">\n";
+    echo "\t\t<td>\nCaracter\n</td>\n";
+    extract($_POST);
+    $dato_usuario = !empty($dato_usuario) ? $dato_usuario : "<a href=\"discusion_2.html\"> No ingreso ningún dato :(";
+    echo "\t\t<td>\n" . $resultado . "\n</td>\n";
+    echo "\t</tr>\n";
+    echo "\t<tr>\n";
+}
+
+?>
